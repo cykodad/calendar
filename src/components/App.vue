c772b202a2ae65d5ff0fd43a5b54266f31f1d92b
Mehmet Bulut <mail@mehmetbulut.de>
2020-09-25 00:12:59 +0200

    D. Calendar day functionality completed

---
+++ src/components/App.vue
@@ -0,0 +1,79 @@
+<template>
+    <div>
+        <div id="day-bar">
+            <div>Mon</div>
+            <div>Tue</div>
+            <div>Wed</div>
+            <div>Thu</div>
+            <div>Fri</div>
+            <div>Sat</div>
+            <div>Sun</div>
+        </div>
+        <div id="calendar">
+            <div v-for="week in weeks" :key=week.id class="calendar-week">
+                <calendar-day v-for="day in week" :key="day.id" :day="day"></calendar-day>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import CalendarDay from "./CalendarDay.vue";
+
+export default {
+    computed: {
+        year() {
+            return this.$store.state.currentYear;
+        },
+        month() {
+            return this.$store.state.currentMonth;
+        },
+        days() {
+
+            // Generate all days in current month
+            let days = [];
+            let currentDay = this.$moment(`${this.year}-${this.month}-1`, "YYYY-M-D");
+
+            do {
+                days.push(currentDay);
+                currentDay = this.$moment(currentDay).add(1, "days");
+            } while(currentDay.month() + 1 === this.month);
+
+            // Add previous days to start
+            const MONDAY = 1;
+            currentDay = this.$moment(days[0]);
+            while (currentDay.day() !== MONDAY) {
+                currentDay = this.$moment(currentDay).subtract(1, "days");
+                days.unshift(currentDay);
+            }
+
+            // Add following days to end
+            const SUNDAY = 0;
+            currentDay = this.$moment(days[days.length -1]);
+            while(currentDay.day() !== SUNDAY) {
+                currentDay = this.$moment(currentDay).add(1, "day");
+                days.push(currentDay);
+            }
+
+            return days;
+        },
+        weeks() {
+            let weeks = [];
+            let week = [];
+
+            for(let day of this.days) {
+                week.push(day);
+                if(week.length === 7) {
+                    weeks.push(week);
+                    week = [];
+                }
+            }
+
+            return weeks;
+        }
+    },
+    components: {
+        CalendarDay
+    }
+}
+</script>
--- src/web.entry.js
+++ src/web.entry.js
@@ -1,8 +1,30 @@
-import Vue from 'vue'
+import Vue from 'vue';
+import "./style.scss";
+
+import VueEx from "vuex";
+Vue.use(VueEx);
+
+import moment from "moment-timezone";
+moment.tz.setDefault("UTC");
+Object.defineProperty(Vue.prototype, "$moment", {get() { return this.$root.moment; }});
+
+import App from "./components/App.vue";
 
 new Vue({
   el: '#app',
   data: {
-    msg: 'Hello World'
+    moment
+  },
+  components: {
+    App
+  },
+  store: {
+    state: {
+      currentYear: 2017,
+      currentMonth: 2
+    },
+    mutations: {
+
+    }
   }
 });
